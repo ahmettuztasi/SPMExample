1facfb156bc98fb727ae3bf29745b1d95453fe33
ahmettuztasi <tuztasiahmet@gmail.com>
2021-04-06 14:12:06 +0300

    SceneDelegate removed

--- SPMExample/ViewController.swift
+++ SPMExample/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import SPMLibrary
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let lib = SPMLibrary()
+        
+        infoLabel.text = lib.text
+        lib.sayHello()
     }
 
 
